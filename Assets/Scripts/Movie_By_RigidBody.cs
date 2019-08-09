893541df651a6a98ae626496000db569a05bd725
KlariaAndrade <Klariah2o@gmail.com>
2019-08-09 09:49:36 -0300

    Upando Tudo

---
+++ Assets/Scripts/Movie_By_RigidBody.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movie_By_RigidBody : MonoBehaviour
+{
+    [SerializeField]
+    private float SpeedFactor  = 10f;
+    private Rigidbody2D Rb;
+    
+    [SerializeField]
+    private float force = 10f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Rb = GetComponent<Rigidbody2D>();    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontalInput = Input.GetAxisRaw("Horizontal");
+        float verticalInput = Input.GetAxisRaw("Vertical");
+
+        Rb.velocity = new Vector3(horizontalInput, verticalInput, 0f) * SpeedFactor; 
+
+        Rb.velocity = new Vector2(horizontalInput * SpeedFactor, Rb.velocity.y);
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            if (Rb.velocity.y == 0f)
+            {
+                Rb.AddForce(Vector3.up * force, ForceMode2D.Impulse);
+            }
+        }        
+    
+    }
+}
---
+++ Assets/Scripts/Simple_Move.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Simple_Move : MonoBehaviour
+{
+     [SerializeField]
+   private float SpeedFactor = 1f;
+   [SerializeField]
+   private float LimitTop = 4f;
+   [SerializeField] 
+   private float LimitBottom = -4f;
+   [SerializeField]
+   private float LimitLeft = -15f;
+   [SerializeField]
+   private float LimitRight = 15f; 
+    
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontalInput = Input.GetAxisRaw("Horizontal");
+        float verticalInput = Input.GetAxisRaw("Vertical");
+
+        Vector3 velocity = new Vector3(horizontalInput, verticalInput, 0f) * SpeedFactor;
+
+        transform.position = transform.position + velocity * Time.deltaTime;
+
+        if (transform.position.y < LimitBottom){
+            transform.position = new Vector3(transform.position.x, LimitBottom, transform.position.z);
+        }
+         if (transform.position.y > LimitTop){
+            transform.position = new Vector3(transform.position.x, LimitTop, transform.position.z);
+        }
+         if (transform.position.x < LimitLeft){
+            transform.position = new Vector3(LimitLeft, transform.position.y, transform.position.z);
+        }
+        if (transform.position.x > LimitRight){
+            transform.position = new Vector3(LimitRight, transform.position.y, transform.position.z);
+        }
+    }
+}
